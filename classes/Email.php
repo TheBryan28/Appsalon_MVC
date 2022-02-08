38433b6123f111decdd156cca57b0930d6f0799d
thebryan228 <78914647+thebryan228@users.noreply.github.com>
2022-02-08 01:19:47 -0500

    mejora de email

--- classes/Email.php
+++ classes/Email.php
@@ -18,7 +18,7 @@ class Email {
     }
 
     public function enviarConfirmacion() {
-
+        $url = $_SERVER['HTTP_HOST'] ===''? '/':$_SERVER['HTTP_HOST'];
          // create a new object
          $mail = new PHPMailer();
          $mail->isSMTP();
@@ -38,18 +38,18 @@ class Email {
 
          $contenido = '<html>';
          $contenido .= "<p><strong>Hola " . $this->email .  "</strong> Has Creado tu cuenta en App Salón, solo debes confirmarla presionando el siguiente enlace</p>";
-         $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
+         $contenido .= "<p>Presiona aquí: <a href='http://".$url."/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
          $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
          $contenido .= '</html>';
          $mail->Body = $contenido;
-
+         debuguear($contenido);
          //Enviar el mail
          $mail->send();
 
     }
 
     public function enviarInstrucciones() {
-
+        $url = $_SERVER['HTTP_HOST'] ===''? '/':$_SERVER['HTTP_HOST'];
         // create a new object
         $mail = new PHPMailer();
         $mail->isSMTP();
@@ -69,7 +69,7 @@ class Email {
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Presiona aquí: <a href='http://".$url."/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
