58d3ba6a6c5a8c3fe0052dfa4ff6923c9189f182
thebryan228 <78914647+thebryan228@users.noreply.github.com>
2022-02-08 01:37:52 -0500

    corregir path

--- Router.php
+++ Router.php
@@ -17,8 +17,9 @@ class Router
 
     public function comprobarRutas() {
         //$currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $currentUrl = $_SERVER['PATH_INFO'] ===''? '/':$_SERVER['PATH_INFO'];
         //si la url esta vacia le asigna /, si no el valor que tiene
-        $currentUrl = $_SERVER['REQUEST_URI'] ===''? '/':$_SERVER['REQUEST_URI'];
+        //$currentUrl = $_SERVER['REQUEST_URI'] ===''? '/':$_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
